dcc4404b80971e0b37f5be2b5ac751bb16ead9a2
Oayz Npk <noppakalo@insightx.in.th>
2025-08-11 10:04:57 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div id="app">
+        <Mainpage />
+    </div>
+</template>
+
+<script setup>
+import Mainpage from './components/Mainpage.vue';
+</script>
---
+++ src/components/Mainpage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container flex items-center justify-center h-svh">
+        <div class="card">
+            <img src="/image-qr-code.png" class="rounded-[10px]" alt="qrcode" />
+            <div class="flex flex-col gap-4">
+                <p class="text-preset1 text-center">
+                    Improve your front-end skills by building projects
+                </p>
+                <p class="text-preset2 text-center">
+                    Scan the QR code to visit Frontend Mentor and take your
+                    coding skills to the next level
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+z
+
+<style scoped>
+.card {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    width: 320px;
+    height: 499px;
+    background-color: white;
+    padding: 16px 16px 40px;
+    border-radius: 20px;
+    box-shadow: 0 25px 25px 0 #0000000c;
+}
+</style>
